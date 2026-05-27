9af3ac278d07d5638f1c2efd66011a50446341d6
willseff <williamli273@gmail.com>
2026-05-27 15:44:36 -0400

    initial commit

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter } from '@next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Inter({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
